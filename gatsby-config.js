fcd52a17745d325d5c9113b6e82933551db944f5
Andrija Pavlovic <andrija.pavlovic996@gmail.com>
2020-12-08 17:14:08 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "",
+        dataset: "",
+      },
+    },
+    "gatsby-plugin-sass",
+  ],
+};
